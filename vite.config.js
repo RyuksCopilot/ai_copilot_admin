306db499f53ff15f227e252008ebd3b78a94d690
Kunal7069 <jaink7069@gmail.com>
2026-01-11 10:30:20 +0530

    websocket done

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
 // export default defineConfig({
 //   plugins: [react()],
-// })
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:8000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["axios"],
+  },
   server: {
     proxy: {
       "/api": {
