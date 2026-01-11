73f3b63b9bd5d2f22a126a52f6617ba0a7f6bb9c
Kunal7069 <jaink7069@gmail.com>
2026-01-11 10:33:56 +0530

    fix: resolve axios explicitly for vite 7

--- vite.config.js
+++ vite.config.js
@@ -20,17 +20,12 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
-  resolve: {
-    alias: {
-      axios: "axios/dist/axios.min.js",
-    },
+  optimizeDeps: {
+    include: ["axios"],
   },
-
   server: {
     proxy: {
       "/api": {
