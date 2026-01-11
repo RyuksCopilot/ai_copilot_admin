2a085a85ab3b2b93d965562af5cead13004f7ae9
Kunal7069 <jaink7069@gmail.com>
2026-01-11 10:46:59 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-
+// https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:8000",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// })
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["axios"],
-  },
   server: {
     proxy: {
       "/api": {
